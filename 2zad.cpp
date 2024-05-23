5a081a094da460eb4d4d4a4c53883ab0df21a775
Mixassss <163171930+Mixassss@users.noreply.github.com>
2024-05-24 00:05:13 +0700

    Комменты

--- 2zad.cpp
+++ 2zad.cpp
@@ -7,58 +7,58 @@
 using namespace std;
 
 vector<int> Eratos(int num) { // решето Эратосфена
-    vector<int> numbersToNum;
-    vector<int> Prostie;
+    vector<int> numbersToNum; //Создает вектор numbersToNum и заполняет его числами от 0 до num - 1.
+    vector<int> Prostie; //Создает вектор Prostie для хранения простых чисел.
     for (int i = 0; i < num; i++) {
-        numbersToNum.push_back(i);
+        numbersToNum.push_back(i); 
     }
 
-    for (int i = 2; i < num; i++) {
+    for (int i = 2; i < num; i++) { //Использует алгоритм "решето Эратосфена": начиная с 2 (первое простое число), отмечает все кратные числа как составные (присваивает им значение 0).
         if (numbersToNum[i] != 0) {
-            for (int j = i + numbersToNum[i]; j < num; j += numbersToNum[i]) {
+            for (int j = i + numbersToNum[i]; j < num; j += numbersToNum[i]) { 
                 numbersToNum[j] = 0;
             }
         }
     }
     for (int i = 0; i < num; i++) {
         if (numbersToNum[i] != 0 && numbersToNum[i] != 1) {
-            Prostie.push_back(numbersToNum[i]);
+            Prostie.push_back(numbersToNum[i]); //После завершения алгоритма, добавляет все ненулевые и не единицу числа в вектор Prostie.
         }
     }
-    return Prostie;
+    return Prostie; //Возвращает вектор Prostie, содержащий все простые числа до num.
 }
 
 int Moded(int number, int m) {
-    int result = 1;
-    for (int i = 0; i < m; i++) {
+    int result = 1; //Инициализирует переменную result со значением 1.
+    for (int i = 0; i < m; i++) { //Выполняет операцию возведения числа number в степень m с использованием цикла.
         result *= number;
-        result %= (m + 1);
+        result %= (m + 1); //После каждого умножения result на number, остаток от деления результата на (m + 1) присваивается самому результату.
     }
-    return result;
+    return result; //Возвращает полученный результат после выполнения всех итераций цикла.
 }
 
 
 int Generator(vector<int> numberProst, int n, vector<int>& number, vector<int>& dividers, int R) { // Генератор
-    int m = (n - 1) / R;
-    int i = 0;
+    int m = (n - 1) / R; //Функция вычисляет m, которое равно (n - 1) / R.
+    int i = 0; //Затем она перебирает простые числа из вектора numberProst
     while (m > 1) {
-        if (m % numberProst[i] == 0) {
+        if (m % numberProst[i] == 0) { //если m делится на текущее простое число, добавляет это простое число в вектор dividers
             dividers.push_back(numberProst[i]);
-            m /= numberProst[i];
+            m /= numberProst[i]; //делит m на это число
         }
         else {
             i++;
         }
     }
 
-    int t = 5;
+    int t = 5; //функция генерирует t случайных чисел в диапазоне от 0 до n - 1
     for (int j = 0; j < t; j++) {
-        number.push_back(rand() % (n));
+        number.push_back(rand() % (n)); //сохраняет их в вектор number
     }
 
-    for (int j : number) {
+    for (int j : number) { //После этого она проверяет, выполняется ли для каждого сгенерированного числа условие
         if ((Moded(j, n - 1) % n) != 1) {
-            return 0;
+            return 0; //Если условие не выполняется, то функция возвращает 0
         }
     }
     return 1;
@@ -67,18 +67,18 @@ int Generator(vector<int> numberProst, int n, vector<int>& number, vector<int>& 
 int Miller(vector<int> numberProst, int n) { // Тест Миллера
     vector<int> dividers;
     vector<int> number;
-    int test = Generator(numberProst, n, number, dividers, 2);
-    if (test == 0) return 0;
+    int test = Generator(numberProst, n, number, dividers, 2); //Использует функцию Generator для генерации случайных чисел и нахождения делителей числа (n - 1)
+    if (test == 0) return 0; //Если тест генератора не прошел, то функция возвращает 0.
     int k = 0;
     for (int j : dividers) {
         for (int z : number) {
-            if ((Moded(z, (n - 1) / j) % n) != 1) {
+            if ((Moded(z, (n - 1) / j) % n) != 1) { //Затем она проходит по всем делителям и проверяет, выполняется ли условие для каждого сгенерированного числа z и делителя j
                 k++;
                 break;
             }
         }
     }
-    if (k == 0) {
+    if (k == 0) { //Если условие не выполняется ни для одного числа, то функция возвращает 0
         return 0;
     }
     return 1;
@@ -114,7 +114,7 @@ int Poli(vector<int> numberProst, int n) { // Тест Поклингтона
 }
 
 int64_t power(int a, int b) { // степень
-    int64_t s = 1;
+    int64_t s = 1; //Функция использует цикл, в котором переменная s умножается на a в каждой итерации
     for (int i = 0; i < b; i++) {
         s = s * a;
     }
@@ -123,10 +123,10 @@ int64_t power(int a, int b) { // степень
 }
 
 int powerMod(int a, int b, int m) { // a^x mod p
-    int s = 1;
+    int s = 1; //Функция вычисляет остаток от деления числа a, возведенного в степень b, на число m
     for (int i = 0; i < b; i++) {
-        s = s * a;
-        s = s % m;
+        s = s * a; //Использует цикл, в котором переменная s умножается на a в каждой итерации
+        s = s % m; //затем берется остаток от деления s на m
     }
     return s;
 }
@@ -135,15 +135,15 @@ int GOST(int t, int q) { // ГОСТ Р 34.10 - 94.
     bool f = false;
     int p = 0;
     while (true) {
-        int N = power(2, t - 1) / q;
-        if (N % 2 == 1)N++;
-        int u = 0;
-        while (true) {
-            p = (N + u) * q + 1;
-            if (power(2, t) < p) {
+        int N = power(2, t - 1) / q; //Алгоритм вычисляет N как power(2, t - 1) / q
+        if (N % 2 == 1)N++; //Если N нечетное, то к нему прибавляется 1
+        int u = 0; //Затем, в цикле, перебираются значения u (начинается с 0, увеличивается на 2 в каждой итерации)
+        while (true) { 
+            p = (N + u) * q + 1; //Для каждого u вычисляется p как (N + u) * q + 1
+            if (power(2, t) < p) { //Если p больше, чем power(2, t), то цикл прерывается
                 break;
             }
-
+//Затем проверяются условия
             if ((power(2, p - 1) % p == 1) && (power(2, N + u) % p != 1)) {
                 f = true;
                 break;
@@ -153,7 +153,7 @@ int GOST(int t, int q) { // ГОСТ Р 34.10 - 94.
         }
 
         if (f) {
-            return p;
+            return p; //Если оба условия выполнены, то функция возвращает значение p
         }
     }
 }
@@ -192,7 +192,7 @@ int main() {
     
     int num = 500;
 
-    vector<int> Prostie = Eratos(num);
+    vector<int> Prostie = Eratos(num); //Использует функцию Eratos для поиска всех простых чисел до num.
     cout << "Простые числа до " << num << ": " << endl;
     for (int i : Prostie) {
         cout << setw(4) << i;
@@ -200,24 +200,24 @@ int main() {
     cout << endl;
     int t = 5;
     int q = 7;
-    if (q < power(2, t / 2 + 1)) {
+    if (q < power(2, t / 2 + 1)) { //Если q меньше, чем power(2, t / 2 + 1), то она использует функцию GOST для генерации простого числа по ГОСТ Р 34.10 - 94 и выводит его на консоль.
         int number = GOST(t, q);
         cout << number << endl;
     }
-    vector<int> ReallyProstie;
-    vector<int> BadOnes;
-    int MissedOnes = 0;
-    int TotalNums = 0;
-    while (ReallyProstie.size() < 10) {
-        int test = rand() % (500 - 4) + 2;
+    vector<int> ReallyProstie; //Создает два вектора: ReallyProstie (для хранения найденных простых чисел)
+    vector<int> BadOnes; //BadOnes (для хранения количества отвергнутых чисел)
+    int MissedOnes = 0; //Cоздает переменную MissedOnes для хранения количества отвергнутых чисел
+    int TotalNums = 0; //Переменную TotalNums для хранения количества проверенных чисел
+    while (ReallyProstie.size() < 10) { //В цикле, пока размер вектора ReallyProstie не станет равен 10, генерируется случайное число
+        int test = rand() % (500 - 4) + 2; //test в диапазоне от 2 до 498
 
-        if (Miller(Prostie, test) && Poli(Prostie, test)) {
-            ReallyProstie.push_back(test);
-            BadOnes.push_back(MissedOnes);
-            MissedOnes = 0;
+        if (Miller(Prostie, test) && Poli(Prostie, test)) { //Затем проверяется, является ли число test простым, используя функции Miller и Poli
+            ReallyProstie.push_back(test); //Если да, то число добавляется в вектор ReallyProstie
+            BadOnes.push_back(MissedOnes); //количество отвергнутых чисел добавляется в вектор BadOnes
+            MissedOnes = 0; //MissedOnes обнуляется
         }
         else {
-            MissedOnes += MillerRabin(test, 10);
+            MissedOnes += MillerRabin(test, 10); //Если число test не является простым, то MissedOnes увеличивается на результат теста Миллера-Рабина
         }
         TotalNums++;
     }
